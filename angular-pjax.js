4af1fb70ac802d3d0a2ab18fad56559c91a63753
Mihail Boguslavsky <unreserved.mail@gmail.com>
2016-08-09 12:28:11 +0300

    Update Library

--- angular-pjax.js
+++ angular-pjax.js
@@ -1,74 +1,145 @@
-(function() {
+(function($) {
 
-  var angularPJAXModule = angular.module('xPJAX', []);
+  var module = angular.module('xPJAX', []);
 
-  var pjaxOptions = {
-    container: '[data-pjax-container]'
+  var defaults = {
+    links: 'a:not([data-remote]):not([data-behavior]):not([data-skip-pjax])'
   };
 
-  angularPJAXModule.provider('pjax', function() {
-    this.config = function(options) {
-      angular.extend(pjaxOptions, options);
-    };
+  var configs = [];
 
+  /**
+   * @param {jQuery|Element|String} container
+   * @returns {jQuery|null}
+   */
+  function getConfig(container) {
+    var $container = $(container);
+
+    if ($container.length === 0) {
+      throw 'Unknown container: ' + container.toString();
+    }
+
+    var found = null;
+
+    angular.forEach(configs, function(value) {
+      var currentContainer = value.container;
+
+      if (currentContainer.get(0) == $container.get(0)) {
+        found = value;
+      }
+    });
+
+    if (found === null) {
+      console.log(container);
+      throw 'Unknown container: ' + container.toString();
+    }
+
+    return found;
+  }
+
+  module.provider('pjax', function() {
     this.$get = function() {
       return {
-        navigateTo: function(url) {
-          $.pjax({
-            url: url,
-            container: pjaxOptions.container
+        defaults: function(options) {
+          if (options) {
+            angular.extend(defaults, options);
+          }
+          return defaults;
+        },
+        containers: function() {
+          var containers = [];
+          angular.forEach(configs, function(value) {
+            containers.push(value.container);
           });
+          return containers;
+        },
+        navigateTo: function(container, url) {
+          var config = getConfig(container);
+
+          return $.pjax({
+            url: url,
+            container: config.container
+          });
+        },
+        reload: function(container, options)
+        {
+          var config = getConfig(container);
+
+          var settings = {};
+          angular.extend(settings, config, options);
+
+          return $.pjax.reload(settings.container, settings);
+        },
+        submit: function(container, event, options)
+        {
+          var config = getConfig(container);
+
+          var settings = {};
+          angular.extend(settings, config, options);
+
+          return $.pjax.submit(event, settings.container, settings);
         }
       };
     };
   });
 
-  angularPJAXModule.directive('pjaxContainer', function() {
-    return {
-      scope:true,
-      controller: ['$rootScope', '$scope', '$window', '$timeout', '$compile', '$element', function($rootScope, $scope, $window, $timeout, $compile, $element) {
-        var contentScope = $scope,
-            parentScope = $scope.$parent;
+  module.directive('pjaxContainer', [
+    '$rootScope',
+    function($rootScope) {
+      return {
+        scope: true,
+        controller: [
+          '$rootScope', '$scope', '$window', '$compile', '$element', '$timeout',
+          function($rootScope, $scope, $window, $compile, $element, $timeout) {
+            var contentScope = $scope,
+                parentScope = $scope.$parent;
 
-        var updateCurrentPath = function() {
-          $rootScope.currentPath = $window.location.pathname;
-        };
+            var $document = $($window.document);
 
-        var $document = $($window.document);
+            var config = angular.extend({}, defaults, {
+              container: $($element)
+            });
 
-        $element.attr('data-pjax-container', true);
+            configs.push(config);
 
-        $document.pjax('a:not([data-remote]):not([data-behavior]):not([data-skip-pjax])', pjaxOptions);
+            $element.attr('data-pjax-container', true);
+            $document.pjax(config.links, config);
 
-        $document.on('pjax:start', function() {
-          $timeout(function() {
-            $rootScope.contentLoading = true;
-          });
-        });
+            $element.on('pjax:start', function(e) {
+              if (!config.container.is(e.target)) {
+                return;
+              }
 
-        $document.on('pjax:beforeReplace', function() {
-          $timeout(function() {
-            if (contentScope) {
-              contentScope.$destroy();
-            }
-          });
-        });
+              $rootScope.contentLoading = true;
+            });
 
-        $document.on('pjax:end', function() {
-          $timeout(function() {
-            updateCurrentPath();
+            $element.on('pjax:beforeReplace', function(e) {
+              if (!config.container.is(e.target)) {
+                return;
+              }
 
-            contentScope = parentScope.$new(false, parentScope);
+              $timeout(function() {
+                if (contentScope) {
+                  contentScope.$destroy();
+                }
+              })
+            });
 
-            $element.html($compile($element.html())(contentScope));
+            $element.on('pjax:end', function(e) {
+              if (!config.container.is(e.target)) {
+                return;
+              }
 
-            $rootScope.contentLoading = false;
-          });
-        });
+              $timeout(function() {
+                contentScope = parentScope.$new(false, parentScope);
+                $element.html($compile($element.html())(contentScope));
+                $rootScope.contentLoading = false;
+              })
+            });
+          }
+        ]
+      };
+    }]
+  );
 
-        updateCurrentPath();
-      }]
-    };
-  });
-
-}).call(this);
+}).call(this, jQuery);
